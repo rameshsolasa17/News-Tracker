fb8b162b9c964c1302b3e042b99ca467f74ce07a
Ramesh Solasa <rameshsolasa17@gmail.com>
2023-09-23 12:03:17 -0500

    new commit

--- app.js
+++ app.js
@@ -1,26 +1,53 @@
 //jshint esversion:6
 
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const multer = require("multer");
+
+
+var date = Date.now();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/Images/');
+  },
+  filename: (req, file, cb) => {
+    //console.log(file);
+    date=Date.now();
+    cb(null, date+path.extname(file.originalname));
+  }
+})
+
+const upload=multer({storage: storage});
+
+
+
+const homeStartingContent = 'Hello'
+const aboutContent = 'Bye'
+const contactContent = 'Tata'
 
 const app = express();
 
 app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://rameshsolasa2001:rameshsolasa@cluster0.tdxhysz.mongodb.net/?retryWrites=true&w=majority", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://rameshsolasa2001:rameshsolasa@cluster0.tdxhysz.mongodb.net/?retryWrites=true&w=majority", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
 const postSchema = {
   title: String,
-  content: String
+  content: String,
+  image: String
 };
 
 const Post = mongoose.model("Post", postSchema);
@@ -39,10 +66,12 @@ app.get("/compose", function(req, res){
   res.render("compose");
 });
 
-app.post("/compose", function(req, res){
+app.post("/compose", upload.single("image"), function(req, res){
+
   const post = new Post({
     title: req.body.postTitle,
-    content: req.body.postBody
+    content: req.body.postBody,
+    image: date+path.extname(req.file.originalname)
   });
 
 
@@ -53,6 +82,25 @@ app.post("/compose", function(req, res){
   });
 });
 
+
+
+app.get("/signin", function(req, res){
+  res.render("signIn");
+});
+
+app.post("/signin", function(req, res) {
+  res.redirect("/");
+});
+
+app.get("/register", function (req, res) {
+  res.render("register");
+})
+
+app.post("/register", function(req, res){
+  //console.log(req.body);
+  res.redirect("/");
+})
+
 app.get("/posts/:postId", function(req, res){
 
 const requestedPostId = req.params.postId;
@@ -60,7 +108,8 @@ const requestedPostId = req.params.postId;
   Post.findOne({_id: requestedPostId}, function(err, post){
     res.render("post", {
       title: post.title,
-      content: post.content
+      content: post.content,
+      image: post.image
     });
   });
 
@@ -75,6 +124,8 @@ app.get("/contact", function(req, res){
 });
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
-});
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, console.log(`Server started on ${PORT}`));
